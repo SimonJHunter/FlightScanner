80052266273ea3c4c61a1b9dae29d9ec7633213b
SimonJHunter <simonjhunter@hotmail.com>
2016-05-27 14:40:13 +0100

    Added basic pax handling

--- FlightScannerApp/FlightScannerApp/js/controllers/FlightPickerController.js
+++ FlightScannerApp/FlightScannerApp/js/controllers/FlightPickerController.js
@@ -8,7 +8,9 @@
             airline: 'BA',
             deptime: new Date('2016', '05', '01', '07', '25'),
             arrtime: new Date('2016', '05', '01', '09', '25'),
-            price: 140
+            price: 140,
+            paxadult: 0,
+            paxchild: 0
         },
         {
             origin: 'Edinburgh',
@@ -16,7 +18,9 @@
             airline: 'Continental',
             deptime: new Date('2016', '05', '01', '10', '40'),
             arrtime: new Date('2016', '05', '01', '15', '00'),
-            price: 450
+            price: 450,
+            paxadult: 0,
+            paxchild: 0
         },
         {
             origin: 'Edinburgh',
@@ -24,8 +28,16 @@
             airline: 'easyJet',
             deptime: new Date('2016', '05', '01', '09', '05'),
             arrtime: new Date('2016', '05', '01', '12', '25'),
-            price: 205
+            price: 205,
+            paxadult: 0,
+            paxchild: 0
         }
-        ]
+    ];
+    $scope.plusOneAdult = function (index) {
+        $scope.flights[index].paxadult += 1;
+    };
+    $scope.minusOneAdult = function (index) {
+        $scope.flights[index].paxadult -= 1;
+    };
 }]);
 
