0697f07e17c453f0d19b06c262d94e02dba3e5d1
SimonJHunter <simonjhunter@hotmail.com>
2016-05-27 12:05:46 +0100

    Basic Picker framework

---
+++ FlightScannerApp/FlightScannerApp/js/controllers/FlightPickerController.js
@@ -0,0 +1,31 @@
+﻿app.controller('FlightPickerController', ['$scope', function($scope){
+
+    $scope.title = 'Your possible flight options';
+    $scope.flights = [
+        {
+            origin: 'Edinburgh',
+            destination: 'Paris',
+            airline: 'BA',
+            deptime: new Date('2016', '05', '01', '07', '25'),
+            arrtime: new Date('2016', '05', '01', '09', '25'),
+            price: 140
+        },
+        {
+            origin: 'Edinburgh',
+            destination: 'New York',
+            airline: 'Continental',
+            deptime: new Date('2016', '05', '01', '10', '40'),
+            arrtime: new Date('2016', '05', '01', '15', '00'),
+            price: 450
+        },
+        {
+            origin: 'Edinburgh',
+            destination: 'Palma',
+            airline: 'easyJet',
+            deptime: new Date('2016', '05', '01', '09', '05'),
+            arrtime: new Date('2016', '05', '01', '12', '25'),
+            price: 205
+        }
+        ]
+}]);
+
