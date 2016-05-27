4e1711f8ff9b1831a9bf2ff733be5adfa0086fbb
SimonJHunter <simonjhunter@hotmail.com>
2016-05-27 16:30:33 +0100

    Berfore moving to directives

--- FlightScannerApp/FlightScannerApp/Scripts/_references.js
+++ FlightScannerApp/FlightScannerApp/Scripts/_references.js
@@ -6,4 +6,6 @@
 /// <reference path="vendor/angular-route.js" />
 /// <reference path="../js/app.js" />
 /// <reference path="../js/controllers/FlightPickerController.js" />
+/// <reference path="vendor/bootstrap.js" />
+/// <reference path="../js/directives/flightinfo.js" />
 // <reference path="vendor/angular-scenario.js" />
